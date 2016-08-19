1e3e6ef3f9381df7500e663289dd1d88fbe62ffc
Pedro Howat <pedro@instant.com.br>
2016-08-19 19:58:35 -0300

    It's alive

--- MultiRedditCreator/ViewController.swift
+++ MultiRedditCreator/ViewController.swift
@@ -10,6 +10,9 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var inputTextView: NSTextView!
+    let multiUrl = "https://www.reddit.com/r/"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +25,35 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func goButtonClicked(sender: NSButton) {
+        guard let text = inputTextView.string else { return }
+        let removeSlash = { (str: String) -> String in
+            return str.stringByReplacingOccurrencesOfString("/", withString: "")
+        }
+
+        let lines: [String] = text.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+        let subs: [String] = lines.filter({ $0.characters.count > 0 }).map({
+            if var range = $0.rangeOfString("/r/") {
+                range.startIndex = range.endIndex
+                range.endIndex = $0.endIndex
+                return removeSlash($0.substringWithRange(range))
+            } else {
+                return removeSlash($0)
+            }
+        })
+
+        let url = multiUrl + subs.joinWithSeparator("+")
+        print(url)
+
+        NSPasteboard.generalPasteboard().clearContents()
+        NSPasteboard.generalPasteboard().setString(url, forType: NSStringPboardType)
+
+        let doneAlert: NSAlert = NSAlert()
+        doneAlert.messageText = "URL copied to clipboard 👍"
+        doneAlert.alertStyle = NSAlertStyle.InformationalAlertStyle
+        doneAlert.addButtonWithTitle("OK")
+        doneAlert.runModal()
+    }
 
 }
 
